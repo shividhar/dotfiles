dedaa20f42752be893295ab4c29b1b875d94c483
Shivansh Dhar <shivi@Shiv-Macbook-Pro.local>
2018-04-19 21:02:25 -0400

    Add more files

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 
 # Install Homebrew
-#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 packages=(
   binutils
@@ -11,8 +11,18 @@ packages=(
   git
   go
   neovim
+  jq
 )
 
 brew install ${packages[@]}
 
+# Setup iTerm2 with custom preferences
+brew cask install iterm2
+
+# Set Font
+cp ../resources/menlo-for-powerline.ttf /Library/Fonts/
+
+cp ../resources/com.googlecode.iterm2.plist ~/Library/Preferences/
+
+# Setup ZSH
 git clone https://github.com/zsh-users/antigen.git ~/antigen
